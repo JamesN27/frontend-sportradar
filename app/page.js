a413de98d6ddcf85723b11305eb540cec8f91428
James Henning <jamesniklas@hotmail.de>
2023-12-10 15:04:03 +0100

    Set up project

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -16,28 +16,11 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
           </a>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.center}></div>
 
       <div className={styles.grid}>
         <a
@@ -91,5 +74,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
