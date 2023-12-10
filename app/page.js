00c8aa309dc96e92fcf7a66826cbc0da6f74ce57
James Henning <jamesniklas@hotmail.de>
2023-12-10 20:52:55 +0100

    Set branch to pre test state

--- app/page.js
+++ app/page.js
@@ -1,14 +1,23 @@
+// pages/MyOverviewPage.js
+'use client';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from '../components/Calendar';
+import EventForm from '../components/EventForm';
 
 function MyOverviewPage() {
+  const [events, setEvents] = useState([]);
+
+  const addEvent = (newEvent) => {
+    setEvents([...events, newEvent]);
+  };
+
   return (
     <div>
       <h1>Event Overview</h1>
-      <Calendar />
-      {/* Place content directly inside Link */}
       <Link href="/eventoverview">Go to Event Overview</Link>
+      <EventForm addEvent={addEvent} />
+      <Calendar events={events} />
     </div>
   );
 }
---
+++ components/EventForm.js
@@ -0,0 +1,72 @@
+// components/EventForm.js
+'use client';
+
+import React, { useState } from 'react';
+
+function EventForm({ addEvent }) {
+  const [title, setTitle] = useState('');
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+  const [teams, setTeams] = useState('');
+  const [sport, setSport] = useState('');
+
+  const sportsOptions = [
+    'Football',
+    'Basketball',
+    'Baseball',
+    'Volleyball',
+    'American Football',
+    'Icehockey',
+    'Handball',
+    'Tennis',
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEvent({ title, start, end, teams, sport });
+    setTitle('');
+    setStart('');
+    setEnd('');
+    setTeams('');
+    setSport('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Home Team"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Away Team"
+        value={teams}
+        onChange={(e) => setTeams(e.target.value)}
+      />
+
+      <select value={sport} onChange={(e) => setSport(e.target.value)}>
+        <option value="" disabled>
+          Select Sport
+        </option>
+        {sportsOptions.map((sportOption, index) => (
+          <option key={index} value={sportOption}>
+            {sportOption}
+          </option>
+        ))}
+      </select>
+
+      <input
+        type="datetime-local"
+        placeholder="Start Date"
+        value={start}
+        onChange={(e) => setStart(e.target.value)}
+      />
+      <button type="submit">Add Event</button>
+    </form>
+  );
+}
+
+export default EventForm;
